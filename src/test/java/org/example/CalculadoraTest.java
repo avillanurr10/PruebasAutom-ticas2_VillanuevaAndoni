fba123532221af23f31961182c0f2da57d65af2d
Andoni <149407392+avillanurr10@users.noreply.github.com>
2025-04-14 09:11:14 +0200

    subida de pruebas Calculadora

--- src/test/java/org/example/CalculadoraTest.java
+++ src/test/java/org/example/CalculadoraTest.java
@@ -1,36 +1,99 @@
 package org.example;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculadoraTest {
+    private Calculadora calc;
 
+    @BeforeEach
+    void prepararCalcudora() {
+        calc = new Calculadora(); // Se ejecuta antes de cada test
+    }
+
+    // 
     @Test
-    void suma() {
+    void sumaDosNumerosPositivos() {
+
+        assertEquals(5, calc.suma(2, 3));
     }
 
     @Test
-    void resta() {
+    void sumaNumeroPositivoYNegativo() {
+
+        assertEquals(4, calc.suma(-1, 5));
     }
 
     @Test
-    void multiplicacion() {
+    void sumaConCero() {
+
+        assertEquals(0, calc.suma(0, 0));
+    }
+
+    // pruebas de restas
+    @Test
+    void restaDosNumerosPositivos() {
+
+        assertEquals(2, calc.resta(5, 3));
     }
 
     @Test
-    void division() {
+    void restaNumeroPositivoYNegativo() {
+
+        assertEquals(-5, calc.resta(10, 15));
+    }
+
+    // pruebas multiplicacion
+    @Test
+    void multiplicacionDosNumerosPositivos() {
+
+        assertEquals(4, calc.multiplicacion(2, 2));
     }
 
     @Test
-    void esPar() {
+    void multiplicacionPorCero() {
+
+        assertEquals(0, calc.multiplicacion(3, 0));
+    }
+
+    // rpuebas de division
+    @Test
+    void divisionValida() {
+        assertEquals(2, calc.division(6, 3));
     }
 
     @Test
-    void maximo() {
+    void divisionPorCeroLanzaExcepcion() {
+        assertThrows(IllegalArgumentException.class, () -> calc.division(5, 0));
     }
 
+    // si es par
+    @Test
+    void numeroPar() {
+        assertTrue(calc.esPar(4));
+    }
+
+    @Test
+    void numeroImpar() {
+        assertFalse(calc.esPar(5));
+    }
+
+    // maximo numero
+    @Test
+    void maximoEntreTresNumeros() {
+
+        assertEquals(3, calc.maximo(1, 2, 3));
+    }
+
+    // si es divisible
     @Test
     void esDivisible() {
+        assertTrue(calc.esDivisible(10, 2));
+    }
+
+    @Test
+    void noEsDivisible() {
+        assertFalse(calc.esDivisible(10, 3));
     }
 }
